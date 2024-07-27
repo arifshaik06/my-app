ed380c781b4ef555b8ff5d8ada9e2c9ad94a98d1
arifshaik06 <arifcherry06@gmail.com>
2024-07-27 15:29:08 +0530

    dynamic form

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,18 +8,43 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class UserComponent implements OnInit {
 
-  public userform:FormGroup = new FormGroup({
+  public userform: FormGroup = new FormGroup({
     name: new FormControl(),
     age: new FormControl(),
-    phone:new FormControl(),
-    email:new FormControl(),
+    phone: new FormControl(),
+    email: new FormControl(),
+    address: new FormGroup({
+      city: new FormControl(),
+      pincode: new FormControl()
+    }),
+    type: new FormControl(),
+    // busfee: new FormControl(),
+    // hostelfee: new FormControl()
   })
 
-  constructor() { }
+  constructor() {
+
+    this.userform.get('type')?.valueChanges.subscribe(
+      (data: any) => {
+        if (data == 'dayscholar') {
+          //add busfee
+          this.userform.addControl('busfee', new FormControl());
+          this.userform.removeControl('hostelfee');
+        }
+        else if (data == 'residental') {
+          //add hostel fee
+
+          this.userform.addControl('hostelfee', new FormControl());
+          this.userform.removeControl('busfee');
+        }
+
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
-  submit(){
+  submit() {
     console.log(this.userform)
   }
 
